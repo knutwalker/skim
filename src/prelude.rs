b6db9aa3df7107d35209d2186ccb088af1d84a2e
Paul Horn <developer@knutwalker.de>
2021-04-06 10:19:51 +0200

    export DefaultSkimItem

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 pub use crate::ansi::AnsiString;
 pub use crate::engine::{factory::*, fuzzy::FuzzyAlgorithm};
 pub use crate::event::Event;
+pub use crate::helper::item::DefaultSkimItem;
 pub use crate::helper::item_reader::{SkimItemReader, SkimItemReaderOption};
 pub use crate::helper::selector::DefaultSkimSelector;
 pub use crate::options::{SkimOptions, SkimOptionsBuilder};
